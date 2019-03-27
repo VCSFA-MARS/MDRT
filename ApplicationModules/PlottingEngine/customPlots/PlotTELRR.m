486ae43cc26cd7753bd1daa31faf0c2d294990ff
Nick Counts <nick.counts@gmail.com>
2019-03-27 16:12:42 -0400

    TEL-RR Valve Plotter - added data selection and auto naming

--- ApplicationModules/PlottingEngine/customPlots/PlotTELRR.m
+++ ApplicationModules/PlottingEngine/customPlots/PlotTELRR.m
@@ -4,7 +4,30 @@
 %   with 6 subplots, x and y axes linked. Puts each valve in its own
 %   subplot.
 
-path = '/Users/engineer/Data Repository/2018-11-16 - NG-10 Launch/data';
+% path = '/Users/engineer/Data Repository/2018-11-16 - NG-10 Launch/data';
+defaultpath = '/Users/engineer/Imported Data Repository/2019-03-27 - NG-11 Prep TELHS-6/data';
+
+path = uigetdir(defaultpath); % No checking implemented yet!
+
+%% Auto generate plot title:
+defaultTitle = 'TEL Rapid Retract Valve Firing';
+    
+    if exist(fullfile(path, 'metadata.mat'),'file')
+        load(fullfile(path, 'metadata.mat'), '-mat');
+        plotTitle = '';
+        if metaData.isOperation
+            plotTitle = [plotTitle, metaData.operationName];
+        end
+        if metaData.isMARSprocedure
+            plotTitle = [plotTitle, ' ', metaData.MARSprocedureName];
+        end
+        
+        plotTitle = [plotTitle, ' ', 'Rapid Retract Valve Firing'];
+    else
+        plotTitle = defaultTitle;
+    end 
+
+
 
 files = {   'TELHS RRFPV1 Device Status.mat';
             'TELHS RRFPV2 Device Status.mat';
@@ -16,7 +39,8 @@ files = {   'TELHS RRFPV1 Device Status.mat';
 f = makeMDRTPlotFigure;
 
 ax = MDRTSubplot(2, 3);
-suptitle('NG-10 TEL Rapid Retract Valve Firing')
+suptitle(plotTitle)
+
 
 for i = 1:numel(files)
     axes(ax(i));
@@ -28,3 +52,4 @@ end
 linkaxes(ax(1:end), 'xy')
 dynamicDateTicks(ax(1:end), 'linked')
 
+ax(1).YLim = [0 5];
