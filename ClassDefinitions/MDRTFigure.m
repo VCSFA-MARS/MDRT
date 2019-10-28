63ddff9b51e96bcfe0c9d1524c73d330a5f97872
Nick Counts <nicholas.counts@vaspace.org>
2019-10-28 10:03:09 -0400

    Started MDRT Plot controller object
    Attempted to subclass figure

---
+++ ClassDefinitions/MDRTFigure.m
@@ -0,0 +1,17 @@
+classdef MDRTFigure < handle
+    %MDRTFigure MDRT Plot-containing figure
+    %   Detailed explanation goes here
+    
+    properties
+        hfig
+        subWindows = [];
+    end
+    
+    methods
+        % Constructor
+        % Register - add spawned windows to the list of subWindows
+        
+    end
+    
+end
+
