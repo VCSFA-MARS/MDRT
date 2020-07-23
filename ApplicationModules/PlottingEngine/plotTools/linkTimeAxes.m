857ac963c44a7e5e3ddbaec5a51284d99629c711
Nick Counts <nicholas.counts@vaspace.org>
2020-07-23 19:41:41 -0400

    Updated linkTimeAxes and unlinkTimeAxes - added documentation

--- ApplicationModules/PlottingEngine/plotTools/linkTimeAxes.m
+++ ApplicationModules/PlottingEngine/plotTools/linkTimeAxes.m
@@ -1,4 +1,20 @@
 function linkTimeAxes( varargin )
+% linkTimeAxes() links x axis (time) on multiple axes in a given figure
+%
+%   This function is called from MDRTFigures with the "Advanced" menu. When
+%   called from the menu, two arguments are passed:
+%   
+%       varargin{1} : matlab.ui.container.Menu
+%       varargin{2} : matlab.ui.eventdata.ActionData
+%
+%   This function will filter out any suptitle handles, which are axes
+%   objects at root. 
+%
+%     linkTimeAxes(menuHandle)
+%     linkTimeAxes(figureHandle)
+%     linkTimeAxes(axesArray)
+
+% Counts - updated 2020
 
 if nargin
     switch class(varargin{1})
@@ -26,5 +42,4 @@ axesArray = rawAxesArray(arrayfun(@(e) ...
                             ~isequal(e.Tag,'suptitle'), ...
                             rawAxesArray));
 
-
-linkaxes(axesArray,'x');
+linkaxes(axesArray, 'x');
