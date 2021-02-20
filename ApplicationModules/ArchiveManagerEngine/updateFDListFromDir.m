d7c5572a43a31de8b5bc31eeca09cc38c50a5c5f
Nick Counts <nick.counts@gmail.com>
2021-02-19 21:30:07 -0500

    Initial code - identifies FD List file and timestamp. Loops through new
    Skeleton - needs business logic and steps to clean out stale array entries

---
+++ ApplicationModules/ArchiveManagerEngine/updateFDListFromDir.m
@@ -0,0 +1,34 @@
+config = MDRTConfig.getInstance
+
+% Get directory listing and discard obvious junk
+files = dir(config.workingDataPath);
+files(ismember({files.name}, {'.', '..', 'metadata.mat'} )) = [];
+
+% Make matrix of dates and array of filenames
+fileDates = [files.datenum]';
+fileNames = {files.name}';
+
+% Find index of AvailableFDs.mat and last modified time
+AFDIDX = find(strcmp([fileNames], 'AvailableFDs.mat'));
+timeHack = fileDates(AFDIDX);
+
+% Remove AvailableFDs from file list
+files(AFDIDX)=[];
+fileDates(AFDIDX)=[];
+fileNames(AFDIDX)=[];
+
+% Load existing FD List
+load(fullfile(config.workingDataPath, 'AvailableFDs.mat'))
+
+
+% Step through each file that has changed
+for i = 1:numel(fileNames)
+    
+    thisFileIndex = ismember(FDList(:,2), fileNames{i});
+    
+
+
+
+end
+
+% TODO: Step through FDList and delete entries without corresponding files
