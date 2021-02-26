a94faf043617452a1622f05f24b82f0770bd5dc6
Nick Counts <nick.counts@gmail.com>
2021-02-26 15:52:56 -0500

    Prunes, updates, and appends FDList variable.
    No function wrapper or file saving. Switched to `matfile()` function for supposed faster file access... we shall see

--- ApplicationModules/ArchiveManagerEngine/updateFDListFromDir.m
+++ ApplicationModules/ArchiveManagerEngine/updateFDListFromDir.m
@@ -1,36 +1,77 @@
-config = MDRTConfig.getInstance
+
+% TODO: Add inputParser to handle forced re-indexing and other fun options
+
+config = MDRTConfig.getInstance;
+dataSetIndexFileName = 'AvailableFDs.mat'; % For future release when this is from a config file
 
 % Get directory listing and discard obvious junk
-files = dir(config.workingDataPath);
-files(ismember({files.name}, {'.', '..', 'metadata.mat'} )) = [];
+    files = dir(config.workingDataPath);
+    files(ismember({files.name}, {'.', '..', 'metadata.mat'} )) = [];
 
 % Make matrix of dates and array of filenames
-fileDates = [files.datenum]';
-fileNames = {files.name}';
+    fileDates = [files.datenum]';
+    fileNames = {files.name}';
 
 % Find index of AvailableFDs.mat and last modified time
-AFDIDX = find(strcmp([fileNames], 'AvailableFDs.mat'));
-timeHack = fileDates(AFDIDX);
+    AFDIDX = find(strcmp([fileNames], 'AvailableFDs.mat'));
+    timeHack = fileDates(AFDIDX);
 
 % Remove AvailableFDs from file list
-files(AFDIDX)=[];
-fileDates(AFDIDX)=[];
-fileNames(AFDIDX)=[];
+    files(AFDIDX)=[];
+    fileDates(AFDIDX)=[];
+    fileNames(AFDIDX)=[];
 
 % Load existing FD List
-load(fullfile(config.workingDataPath, 'AvailableFDs.mat'))
+    load(fullfile(config.workingDataPath, dataSetIndexFileName))
 
-
+% Index initialization
+    iFilesToAdd=false(numel(fileNames), 1);
+    iRowsToRemove=true(length(FDList), 1);
+    newFDListEntries={};
+    workingFDList=FDList;
+    
 % Step through each file that has changed
 for i = 1:numel(fileNames)
     
-    thisFileIndex = ismember(FDList(:,2), fileNames{i});
+    iThisFileInDir = ismember(FDList(:,2), fileNames{i});
+    mf = matfile(fullfile(config.workingDataPath, fileNames{i} ));
+        
+    if ~any(iThisFileInDir) % ---------------- Filename not found in FDList
+
+        % Add info to "newFDListEntries" for merging later
+        if isprop(mf, 'fd')
+            fd = mf.fd; % Actually loads file
+            newFDListEntries = vertcat(newFDListEntries, { fd.FullString, fileNames{i} });
+        end
+
+    else % --------------------------------------- Filename found in FDList
     
+        iRowsToRemove(i) = false; % In FDList and Dir. 
+        
+        if fileDates(i) > timeHack % ------------------------ File is newer
+        
+            if isprop(mf, 'fd') % update the appropriate row
+                fd = mf.fd; % Actually loads file
+                workingFDList(iThisFileInDir,:) = { fd.FullString, fileNames{i} };
+            end
+            
+        else % ---------------------------------------------- File is older
+           
+            % Nothing to do - let it go!
+            
+        end
 
-
-
+        % File in FDList but not in directory?
+            % Nothing to do here. After loop, clean
+    
+    end
+    
 end
 
-% TODO: Step through FDList and delete entries without corresponding files
+% Rmove invalid FDList Entries
+    workingFDList(iRowsToRemove, :) = [];
+% Append new FDList Entries
+    workingFDList = vertcat(workingFDList, newFDListEntries);
 
-% Return FDList array
+FDList = workingFDList
+
