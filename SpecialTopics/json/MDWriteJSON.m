56567b13a72e85fcfd69bf76e1aab4a38ab0056d
Nick Counts <nick.counts@gmail.com>
2021-03-06 00:14:51 -0500

    Added hooks to switch between MATLAB native JSON and 3rd party library
    Adding code and version number so we don't forget later

--- SpecialTopics/json/MDWriteJSON.m
+++ SpecialTopics/json/MDWriteJSON.m
@@ -1,7 +1,12 @@
 function [ out ] = MDWriteJSON( name, object, filename, varargin )
 %MDWriteJSON saves a variable or object as a JSON text file.
 
-    out = savejson( name, object, filename );
+%     if verLessThan('matlab', '9.1')
+%     else   
+%         % MATLAB builtin ?
+%     end
+
+        out = savejson( name, object, filename );
 
 end
 
