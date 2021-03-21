f18f7344273aa6f5fdf49ccfb0395c4e8e741e9d
Nick Counts <nick.counts@gmail.com>
2021-03-21 17:56:51 -0400

    Added code to fix struct arrays in graph configs
    JSONLab appears to break reading struct arrays. Added specific code to MDRT wrapper to check for graph structs and clean up the import/export

--- SpecialTopics/json/MDReadJSON.m
+++ SpecialTopics/json/MDReadJSON.m
@@ -6,6 +6,41 @@ function [ data ] = MDReadJSON( filename, varargin )
 %         % MATLAB builtin ?
 %     end    
 
-data = loadjson( filename );
+data = loadjson( filename, 'SimplifyCell', 1);
+
+switch lower(char(fieldnames(data)))
+    case 'graph'
+        debugout('Detected graph struct, translating')
+        if iscell(data.graph.streams)
+            data.graph.streams = [data.graph.streams{:}];
+            
+            % Fix empty cell in "toPlot" struct array
+            for si = 1:numel(data.graph.streams)
+                if isempty(data.graph.streams(si).toPlot)
+                    debugout('Fixing empty toPlot struct')
+                    data.graph.streams(si).toPlot = cell(0);
+                end
+            end
+            
+            % Fix Time and String structs?
+            if ~isstruct(data.graph.time.startTime)
+                if isempty(data.graph.time.startTime)
+                    data.graph.time.startTime = struct([]);
+                end
+            end
+            
+            if ~isstruct(data.graph.time.stopTime)
+                if isempty(data.graph.time.stopTime)
+                    data.graph.time.stopTime = struct([]);
+                end                
+            end
+            
+            data = data.graph;
+            return
+        end
+        
+    otherwise
+        
+end
 
 end
--- SpecialTopics/json/MDWriteJSON.m
+++ SpecialTopics/json/MDWriteJSON.m
@@ -6,7 +6,19 @@ function [ out ] = MDWriteJSON( name, object, filename, varargin )
 %         % MATLAB builtin ?
 %     end
 
-        out = savejson( name, object, filename );
+    if isstruct(object)
+        switch checkStructureType(object)
+            case 'graph'
+                debugout('Writing MDRT Graph to JSON')
+                out = savejson('graph', object, 'FileName', filename, 'FloatFormat', '%.15g' );
+                return
+            otherwise
+                
+        end
+    end
+    
+    debugout('writing to JSON')
+    out = savejson( '', object, filename );
 
 end
 
