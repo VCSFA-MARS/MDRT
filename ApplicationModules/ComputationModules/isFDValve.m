a5673ad022830e882eb4c1abe8b0ea0d7aac9c90
Nick Counts <nick.counts@gmail.com>
2021-06-15 19:41:29 -0400

    New Function: check if FD is valve data

---
+++ ApplicationModules/ComputationModules/isFDValve.m
@@ -0,0 +1,36 @@
+function isValve = isFDValve(fd)
+% isFDValve takes an FD structure or a filename and returns true if the fd
+% is valve data. 
+
+isValve = [];
+
+switch class(fd)
+    case 'struct'
+        if strcmpi(checkStructureType(fd), 'fd')
+            testStr = fd.ts.Name;
+        else
+            return
+        end
+        
+    case 'char'
+        % filename passed as char array
+        testStr = fd;
+        
+    case 'cell'
+        % passed as cell array?
+        testStr = fd{1};
+        
+    otherwise
+        return
+end
+        
+mustHave = {'Damper|Positioner|Valve|[D|P]CVN[OC]|RV'};
+mustNotHave = { 'Close|Open|Var|Percent|Pump|Fan|__' };
+excludeValves = { 'RV-000[15678]|WDS PCR|Shut-Out'} ;
+
+
+l_allValves = ~cellfun('isempty',regexp(testStr, mustHave));
+l_toExclude = ~cellfun('isempty',regexp(testStr, mustNotHave));
+l_notValves = ~cellfun('isempty',regexp(testStr, excludeValves));
+
+isValve = l_allValves & ~l_toExclude & ~l_notValves;
