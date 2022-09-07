76221f7e3817f75895c792b3ffdf4c031a008c5e
Nick <nicholas.counts@vaspace.org>
2022-09-07 11:58:57 -0400

    Added matlab/excel date functions to remove toolbox dependency

---
+++ overloads/m2xdate.m
@@ -0,0 +1,9 @@
+function [ xdates ] = m2xdate( mdates )
+%x2mdate Summary of this function goes here
+%   Detailed explanation goes here
+
+xdates = mdates - 693960;
+
+
+end
+
---
+++ overloads/x2mdate.m
@@ -0,0 +1,9 @@
+function [ mdates ] = x2mdate( xdates )
+%x2mdate Summary of this function goes here
+%   Detailed explanation goes here
+
+mdates = xdates + 693960;
+
+
+end
+
