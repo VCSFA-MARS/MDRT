83edb53a2c102c2a5d7e3a15a57f9b9d14b05720
Nick Counts <nick.counts@gmail.com>
2025-06-20 11:54:15 -0400

    Updated load/save fd helpers: added location overrides and documentation

--- helpers/save_fd_to_disk.m
+++ helpers/save_fd_to_disk.m
@@ -1,18 +1,55 @@
-function save_fd_to_disk(fd)
+function save_fd_to_disk(fd, varargin)
 %save_fd_by_name saves an fd struct in the currently selected data set
-%with an appropriate filename
+%with an appropriate filename.
+%
+%This function responds to the version field in the fd struct, saving for
+%matfile() streaming if not legacy version (v1)
+%
+%Accepts 'folder' argument to save to the specified location. If omitted, 
+%the file is saved to the currently selected data set
 
-% config = MDRTConfig.getInstance;
-config = getConfig;
+ORIG_VERSION = {'', 'v1'};
+is_save_for_streaming = true;
 
-filename = fullfile(config.dataFolderPath, ...
-    [makeFileNameForFD(fd), '.mat'] );
 
-% if ~exist(filename, "file")
-%     fprintf('File not found: %s\n', filename)
-%     error('Tried to load non-existant FD: %s', fd)
-% end
+%% Process optional arguments
 
-save(filename, 'fd');
+if ~length(varargin) > 0
+    config = getConfig;
+    dest_folder = config.dataFolderPath;
+else
+    for i = 1:2:length(varargin)
+        key = varargin{i};
+        value = varargin{i+1};
+        switch lower(key)
+            case {'folder', 'destination', 'dest', 'path', 'location'}
+                assert(isfolder(value));
+                dest_folder = value;
+        end
+    end
+end
+
+
+%% Build fullfile path/name
+
+file_str = [makeFileNameForFD(fd), '.mat'];
+filename = fullfile(dest_folder, file_str );
+
+
+%% Check version and set behavior flags
+if isfield(fd, 'version')
+    if ismember(fd.version, ORIG_VERSION)
+        is_save_for_streaming = false;
+    end
+end
+
+
+%% Write to disk in version-dependent format
+if is_save_for_streaming == false
+    save(filename, 'fd');
+elseif is_save_for_streaming
+    save(filename, '-struct', 'fd')
+end
+
 
 end
