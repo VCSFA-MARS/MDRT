00c336d438efa930922aed14edeaaca7ccfc53e2
Nick Counts <nick.counts@gmail.com>
2025-06-20 08:45:05 -0400

    Documented pressure cycle data table generator script

--- SpecialTopics/pressureCycles.m
+++ SpecialTopics/pressureCycles.m
@@ -1,36 +1,76 @@
-% Data massager for storage vessel pressure cycle analysis
-% This can be stuffed into a function/gui for easier analysis.
+%% Data massager for storage vessel pressure cycle analysis
+% This can be stuffed into a function/gui for easier analysis in the future
+% but for now it is a fairly manual process.
+%
+% Point the tool at an archive
 
-% Sorry for the hardcoded fd struct. 
 
-input_data_file = '~/data/import/2023-01-02 - All MPGN2 Storage Pressure Data/data/5902 GN2 PT-5902 Press Sensor Mon - Filtered.mat';
-output_excel_file = '~/Downloads/2023 MP GN2 Pressure.xlsx';
+%% Load the data file and generate the time and data variables
+config = MDRTConfig.getInstance();
+
+dataPath = config.workingDataPath;
+
+[loadFileName_str, loadFilePath] = uigetfile( {...
+                '*.mat', 'MDRT Data File'; ...
+                '*.*',     'All Files (*.*)'}, ...
+                'Pick a file', fullfile(dataPath, '*.mat'));
+
+input_data_file = fullfile(loadFilePath, loadFileName_str);
 
 load(input_data_file);
 t = fd.ts.Time;
 p = fd.ts.Data;
 
-lmax = islocalmax(p, "MinSeparation", 100, "MinProminence", 100);
-lmin = islocalmin(p, "MinSeparation", 100, "MinProminence", 100);
 
-lring = islocalmax(p, "MinSeparation", 100);
+%% Find the peaks and valleys for waterfall analysis
+%
+% Adjusting the sensor range is the shortcut for setting the "minimum
+% prominence" magnitude for local peak detection/rejection.
+%
+% Run this section of code and observe the plot to sanity check the results
+
+SENSOR_RANGE = 4000;
+MPROM = SENSOR_RANGE * 0.05;
+MSEP = 50;
+
+lmax = islocalmax([-Inf; p; -Inf], "MinSeparation", MSEP, "MinProminence", MPROM, "FlatSelection","center");
+lmin = islocalmin([ Inf; p;  Inf], "MinSeparation", MSEP, "MinProminence", MPROM, "FlatSelection","center");
+
+lmax = lmax(2:end);
+lmin = lmin(2:end);
+
+lring = islocalmax(p, "MinSeparation", MSEP);
 
 
-figure();
-plot(t, p, 'DisplayName', 'MP GN2 Storage Pressure Peak Analysis');
+makeMDRTPlotFigure();
+plot(t, p, 'DisplayName', 'TELHS Pressure Peak Analysis');
+title(fd.FullString);
 hold on;
 
 % pr = plot(t(lring), p(lring), 'LineStyle','none', 'Marker','*','Color','magenta');
-pp = plot(t(lmax), p(lmax), 'LineStyle','none', 'Marker','*','Color','r');
-pq = plot(t(lmin), p(lmin), 'LineStyle','none', 'Marker','*','Color','r');
+pp = plot(t(lmax), p(lmax), 'LineStyle','none', 'Marker','^','Color','r');
+pq = plot(t(lmin), p(lmin), 'LineStyle','none', 'Marker','v','Color','r');
 
 dynamicDateTicks;
 plotStyle;
 
 lall = lmax | lmin;
+%% Save the data to an excel file
+%
+% Once the previous section completes with a satisfactory set of peaks and
+% valleys, you can save the data. The following lines generate the
+% spreadsheet's file name. The top line is for typical FCS pressure sensors
+% and the bottom line is for typical TELHS. Uncomment the appropriate line
+% and then run this section to generate the output file.
+
+excel_file = sprintf('Pressure %s-%s.xlsx', fd.Type, fd.ID);
+% excel_file = sprintf('Pressure %s.xlsx', fd.Type);
+
+output_excel_file = fullfile(dataPath, '..', excel_file);
 
 press_table = table(cellstr(datestr(t(lall))), ...
                     m2xdate(t(lall)), p(lall),  ...
                     'VariableNames',{'DateString', 'ExcelDateNum', 'Pressure'})
 
-writetable(press_table, output_excel_file, 'Sheet',1)
+% writetable(press_table, output_excel_file, 'Sheet',1)
+writetable(press_table, output_excel_file)
