c1b9c71fe8475038a8ba584c2111aa7ca9a6f6ee
Nick Counts <nick.counts@gmail.com>
2025-06-20 14:21:11 -0400

    load_fd_by_name: added option to specify filename & docs

--- helpers/load_fd_by_name.m
+++ helpers/load_fd_by_name.m
@@ -4,21 +4,41 @@ function fd = load_fd_by_name(fd_str, varargin)
 %
 %Accepts 'folder' argument to load from the specified location. If omitted, 
 %the file is loaded from the currently selected data set
+%
+%   fd = load_fd_by_name('An FD String', 'folder', '/my/data/folder');
+%
+%Accepts 'isFilename' argument (default is false). If true, then the
+%fd_str is treated as the filename instead of the fd string. Allows you to
+%use the function to open a particular file without knowing the fd string.
+%
+%   fd = load_fd_by_name('my_file', 'isFilename', true);
+%
+%If no .mat extension is given, one will be appended. If a different
+%extension is given, it will be changed to .mat
+%
+%When specifying by filename, if there is a path in the filename string, it
+%will be discarded in favor of the current data set or the 'folder'
+%argument.
+
+%% Defaults
+
+use_filename = false;
+
+config = getConfig;
+containing_folder = config.dataFolderPath;
+
 
 %% Process optional arguments
 
-if ~length(varargin) > 0
-    config = getConfig;
-    containing_folder = config.dataFolderPath;
-else
-    for i = 1:2:length(varargin)
-        key = varargin{i};
-        value = varargin{i+1};
-        switch lower(key)
-            case {'folder', 'destination', 'dest', 'path', 'location'}
-                assert(isfolder(value));
-                containing_folder = value;
-        end
+for i = 1:2:length(varargin)
+    key = varargin{i};
+    value = varargin{i+1};
+    switch lower(key)
+        case {'folder', 'destination', 'dest', 'path', 'location'}
+            assert(isfolder(value));
+            containing_folder = value;
+        case {'isfilename','byfilename', 'byname'}
+            use_filename = value;
     end
 end
 
@@ -26,7 +46,29 @@ end
 
 %% Build fullfile path/name
 
-file_str = [makeFileNameForFD(fd_str), '.mat'];
+if iscellstr(fd_str)
+    while iscellstr(fd_str) % unwrap the cellstr
+        fd_str = fd_str{1};
+    end
+end
+assert(ischar(fd_str))
+
+if use_filename
+    [~,name,ext] = fileparts(fd_str);
+    % Add .mat extension if missing
+    if isempty(ext)
+        fd_str = [name, '.mat'];
+    else
+        % Replace bad extension with .mat
+        if ~strcmpi(ext, '.mat')
+            fd_str = [name, '.mat'];
+        end
+    end
+    file_str = fd_str;
+else
+    file_str = [makeFileNameForFD(fd_str), '.mat'];
+end
+
 filename = fullfile(containing_folder, file_str );
 
 
