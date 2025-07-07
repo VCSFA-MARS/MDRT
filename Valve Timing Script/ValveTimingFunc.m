147aef5fe91e59e20628a22d574c9a20c6f81af4
Nick Counts <nick.counts@gmail.com>
2025-07-07 17:43:23 -0400

    Tweaked valveTimingFunc error message generation and file paths

--- Valve Timing Script/ValveTimingFunc.m
+++ Valve Timing Script/ValveTimingFunc.m
@@ -6,14 +6,14 @@ function ValveTimingFunc(DataPath,ExportFileName)
 %   This function automatically performs valve timing calculations for
 % discrete valves on Pad 0C. The function is intended for integration with
 % the 'review.m' data review tool, but may operate independently. The
-% function takes as an input the file location of .mat files intended for 
-% processing; these .mat files are attained through the Data Review GUI 
+% function takes as an input the file location of .mat files intended for
+% processing; these .mat files are attained through the Data Review GUI
 % tool in 'review.m'.
-%   The function processes and organizes the fd structures associated with 
-% these .mat files. The function compares the data provided against a 
-% directory of I/O Codes and then again to a list of MARS FN's and their 
-% associated I/O Codes. This allows for the funamental .mat data to be 
-% associated with the appropriate signals (command, open, closed) for the 
+%   The function processes and organizes the fd structures associated with
+% these .mat files. The function compares the data provided against a
+% directory of I/O Codes and then again to a list of MARS FN's and their
+% associated I/O Codes. This allows for the funamental .mat data to be
+% associated with the appropriate signals (command, open, closed) for the
 % appropriate valves. If data for a specific valve is not provided, the
 % function will note that in its output.
 %   Once the provided data is organized, computation of valve timing is
@@ -27,7 +27,7 @@ function ValveTimingFunc(DataPath,ExportFileName)
 %   -> Be wary of changes made to the .xlsx or .xltx files listed below in
 %       'Supporting Files' -- this function is sensitive to the formatting
 %       of those files, and changes to the files may necessitate changes to
-%       the code. Additional rows may be added to Valve Directory or Valve 
+%       the code. Additional rows may be added to Valve Directory or Valve
 %       Grouping so long as all columns are populated correctly.
 %   -> If a 'The following I/O Codes afre missing data:' error is returned
 %       unexpectedly, you may look at the Error Table for a more detailed
@@ -70,15 +70,16 @@ function ValveTimingFunc(DataPath,ExportFileName)
 %   -> adjust date when finally published
 % -------------------------------------------------------------------------
 
+TEMPLATE_DIR = 'Valve Timing Script';
 
 % -------------------------------------------------------------------------
 % We import the I/O Code Directory and Valve Grouping List (importing
 % .xslx files, storing as tables).
 % -------------------------------------------------------------------------
 Directory = readtable('Pad0C_ValveDirectory.xlsx', ...
-    'PreserveVariableNames',true,'Sheet','Channel List');
+  'PreserveVariableNames',true,'Sheet','Channel List');
 GroupList = readtable('Pad0C_ValveGrouping.xlsx', ...
-    'PreserveVariableNames',true);
+  'PreserveVariableNames',true);
 
 QuickSearch = Directory{:,'I/O Code'};
 % -------------------------------------------------------------------------
@@ -90,10 +91,10 @@ QuickSearch = Directory{:,'I/O Code'};
 % -------------------------------------------------------------------------
 % We generate a progress bar for the import process.
 ImportProgressBar = waitbar(0,'Importing and Organizing Data', ...
-    'WindowStyle','modal');
+  'WindowStyle','modal');
 
 % We list the folder contents from the specified folder location.
-files = dir(DataPath);
+files = dir(fullfile(DataPath, '*.mat'));
 
 % We generate an empty structure for the extracted time series.
 MasterStructure = struct('Code',[],'TimeSeries',[]);
@@ -101,48 +102,48 @@ MasterStructure = struct('Code',[],'TimeSeries',[]);
 % We create an error table to store information regarding which files
 % could not be processed.
 ErrorTable = table('Size',[length(files) 2],'VariableTypes', ...
-    {'string' 'string'},'VariableNames',{'Tag' 'Error'});
+  {'string' 'string'},'VariableNames',{'Tag' 'Error'});
 
 for i = 1:length(files)
-
-    % We pull the name of the current .mat file.
-    currName = erase(files(i).name,".mat");
-
-    % We pull the structure data from the current .mat file.
-    if files(i).isdir == 0
-        currStruct = load(strcat(DataPath,'\',files(i).name));
-    else
-        continue
-    end
-
-    % We filter against the Pad 0C I/O Code Directory.
-    if max(strcmp(currName,QuickSearch)) == 0
-        ErrorTable.Tag(i) = currName;
-        ErrorTable.Error(i) = ['Tag is not found in the I/O Code ' ...
-            'Directory.'];
-        continue
-    else
-    end
-    
-    % We pull the fd structure.
-    if isfield(currStruct,'fd') == 1
-        currTag = currStruct.fd;
-    else
-        ErrorTable.Tag(i) = currName;
-        ErrorTable.Error(i) = 'Tag does not contain an fd structure.';
-        continue
-    end
-
-    % Here: either save the data (currName, currTag, and currTime) in a 
-    % separate structure, for manipulation outside of the for-loop, OR run
-    % valve timing computations within the for-loop and save data within 
-    % the for-loop as well.
-    MasterStructure(end+1).Code = find(strcmp(currName,QuickSearch) == 1);
-    MasterStructure(end).TimeSeries = currTag.ts;
-
-    % We update the progress bar.
-    waitbar(i/length(files))
-
+  
+  % We pull the name of the current .mat file.
+  currName = erase(files(i).name,".mat");
+  
+  % We pull the structure data from the current .mat file.
+  if files(i).isdir == 0
+    currStruct = load(fullfile(DataPath,files(i).name));
+  else
+    continue
+  end
+  
+  % We filter against the Pad 0C I/O Code Directory.
+  if max(strcmp(currName,QuickSearch)) == 0
+    ErrorTable.Tag(i) = currName;
+    ErrorTable.Error(i) = ['Tag is not found in the I/O Code ' ...
+      'Directory.'];
+    continue
+  else
+  end
+  
+  % We pull the fd structure.
+  if isfield(currStruct,'fd') == 1
+    currTag = currStruct.fd;
+  else
+    ErrorTable.Tag(i) = currName;
+    ErrorTable.Error(i) = 'Tag does not contain an fd structure.';
+    continue
+  end
+  
+  % Here: either save the data (currName, currTag, and currTime) in a
+  % separate structure, for manipulation outside of the for-loop, OR run
+  % valve timing computations within the for-loop and save data within
+  % the for-loop as well.
+  MasterStructure(end+1).Code = find(strcmp(currName,QuickSearch) == 1);
+  MasterStructure(end).TimeSeries = currTag.ts;
+  
+  % We update the progress bar.
+  waitbar(i/length(files))
+  
 end
 
 % We remove empty rows from the error table.
@@ -162,31 +163,32 @@ close(ImportProgressBar)
 % We create the export file.
 % -------------------------------------------------------------------------
 % We remove any file extension included in the provided export file name.
-[~,ExportFileName,~] = fileparts(ExportFileName);
+[ExportPath,ExportFileName,~] = fileparts(ExportFileName);
 
 % We add a .xlsx designator to the export file name.
 ExportName = strcat(ExportFileName,'.xlsx');
+ExportName = fullfile(ExportPath, ExportName);
 
 %We define the folder path of the results template Excel sheet.
-TemplateName = 'Pad0C_ValveTimingExportTemplate.xltx';
+TemplateName = fullfile(TEMPLATE_DIR, 'Pad0C_ValveTimingExportTemplate.xltx');
 
 % We create a copy of the template with the desired name.
-copyfile(TemplateName,ExportName)
+[stat, msg, msgid] = copyfile(TemplateName,ExportName)
 
 % We create a table representing the blank export Excel sheet.
 ExportData = readtable(TemplateName,'PreserveVariableNames',true);
 
 % We specify that the Errors variable required string data.
 ExportData = convertvars(ExportData,'Errors','string');
-% ------------------------------------------------------------------------- 
+% -------------------------------------------------------------------------
 
 
-% ------------------------------------------------------------------------- 
+% -------------------------------------------------------------------------
 % We introduce a wait bar to track calculations being done on each valve.
-% ------------------------------------------------------------------------- 
+% -------------------------------------------------------------------------
 ProcessProgressBar = waitbar(0,'Beginning Computational Process', ...
-    'WindowStyle','modal');
-% ------------------------------------------------------------------------- 
+  'WindowStyle','modal');
+% -------------------------------------------------------------------------
 
 
 % -------------------------------------------------------------------------
@@ -197,156 +199,161 @@ ProcessProgressBar = waitbar(0,'Beginning Computational Process', ...
 %   -> ValveTimingComputations.m
 % -------------------------------------------------------------------------
 for i = 1:height(GroupList)
-
-    % We define currValve and currCode for the
-    currType = string(GroupList{i,'Valve Type'});
-    currValve = string(GroupList{i,'Valve FN'});
-    currOpenCode = GroupList{i,'Open I/O'};
-    currClosedCode = GroupList{i,'Closed I/O'};
-    currCommandCode = GroupList{i,'Command I/O'};
-
-    % We update the progress bar.
-    waitbar(i/height(GroupList),ProcessProgressBar, ...
-        strcat('Computing For:',{' '},currType,'-',currValve));
-
-    % We confirm that all three codes are present in MasterStructure.
-    CodeCheck = transpose([MasterStructure.Code]);
-    OpenCheck = ismember(currOpenCode,CodeCheck);
-    ClosedCheck = ismember(currClosedCode,CodeCheck);
-    CommandCheck = ismember(currCommandCode,CodeCheck);
-    ExportError = 'The following I/O Codes are missing data:';
-
-    % We list in the export file which I/O codes are missing.
-    if OpenCheck == 0 || ClosedCheck == 0 || CommandCheck == 0
-        if OpenCheck == 0
-            ExportError = strcat(ExportError,currOpenCode);
-        end
-        if ClosedCheck == 0 && OpenCheck == 0
-            ExportError = strcat(ExportError,',',num2str(currClosedCode));
-        elseif ClosedCheck == 0 && OpenCheck == 1
-            ExportError = strcat(ExportError,currClosedCode);
-        end
-        if CommandCheck == 0 && (OpenCheck == 0 || ClosedCheck == 0)
-            ExportError = strcat(ExportError,',',num2str(currCommandCode));
-        elseif CommandCheck == 0 && (OpenCheck == 1 && ClosedCheck == 1)
-            ExportError = strcat(ExportError,num2str(currCommandCode));
-        end
-        ExportData.Errors(i) = convertCharsToStrings(ExportError);
-        continue
+  
+  % We define currValve and currCode for the
+  currType = string(GroupList{i,'Valve Type'});
+  currValve = string(GroupList{i,'Valve FN'});
+  currOpenCode = GroupList{i,'Open I/O'};
+  currClosedCode = GroupList{i,'Closed I/O'};
+  currCommandCode = GroupList{i,'Command I/O'};
+  
+  % We update the progress bar.
+  waitbar(i/height(GroupList),ProcessProgressBar, ...
+    strcat('Computing For:',{' '},currType,'-',currValve));
+  
+  % We confirm that all three codes are present in MasterStructure.
+  CodeCheck = transpose([MasterStructure.Code]);
+  OpenCheck = ismember(currOpenCode,CodeCheck);
+  ClosedCheck = ismember(currClosedCode,CodeCheck);
+  CommandCheck = ismember(currCommandCode,CodeCheck);
+  ExportError = 'The following I/O Codes are missing data:';
+  
+  ErrorMsg = '';
+  MissingFDs = '';
+  
+  if ~all([OpenCheck, ClosedCheck, CommandCheck])
+    ErrorMsg = ExportError;
+    if ~OpenCheck
+      MissingFDs = sprintf('%s %d,', MissingFDs, currOpenCode);
     end
-
-    % We pull the time series for the three codes.
-    currOpenData = MasterStructure(currOpenCode == CodeCheck).TimeSeries;
-    currClosedData = MasterStructure(currClosedCode == ...
-        CodeCheck).TimeSeries;
-    currCommandData = MasterStructure(currCommandCode == ...
-        CodeCheck).TimeSeries;
-
-    % We normalize open state data, as necessary.
-    if max(currOpenData.Data) > 1
-        currOpenData.Data = currOpenData.Data ./ max(currOpenData.Data);
+    if ~ClosedCheck
+      MissingFDs = sprintf('%s %d,', MissingFDs, currClosedCode);
     end
-
-    % We normalize closed state data, as necessary.
-    if max(currClosedData.Data) > 1
-        currClosedData.Data = currClosedData.Data ./ max( ...
-            currClosedData.Data);
+    if ~CommandCheck
+      MissingFDs = sprintf('%s %d', MissingFDs, currCommandCode);
     end
-
-    % We normalize command data, as necessary.
-    if max(currCommandData.Data) > 1
-        currCommandData.Data = currCommandData.Data ./ max( ...
-            currCommandData.Data);
+    if MissingFDs(end) == ','
+      MissingFDs(end) = [];
     end
     
-    % We verify that all three time series begin at the same time stamp.
-    if currOpenData.Time(1) ~= currCommandData.Time(1) || ...
-            currClosedData.Time(1) ~= currCommandData.Time(1)
-        ExportError = ['Datum times are inconsistent across data. ' ...
-            'Cannot compute timing.'];
-        ExportData.Errors(i) = convertCharsToStrings(ExportError);
-        continue
-    end
-
-    % We assign logical values to TRUE and FALSE states depending on the
-    % state signal structure, which may vary from valve-to-valve.
-    TRUE = GroupList{i,'TRUE State Signal'};
-    FALSE = TRUE == 0;
-
-    % We look for instances of OPEN -> CLOSE commands.
-    CommandCloseIndices = find(currCommandData.Data == 0);
-    ClosedStateOpenIndices = find(currClosedData.Data == TRUE);
-    CommandSwitchIndices = zeros(1,3);
-    StateSwitchIndices = zeros(1,3);
-    AveragingVector = zeros(1,3);
-
-    for j = 1:length(CommandCloseIndices)
-        if CommandCloseIndices(j) == 1
-            continue
-        elseif currCommandData.Data(CommandCloseIndices(j) - 1) == 1
-            for k = 1:length(CommandSwitchIndices)
-                if CommandSwitchIndices(k) == 0
-                    CommandSwitchIndices(k) = CommandCloseIndices(j) - 1;
-                    break
-                end
-            end
+    ErrorMsg = sprintf('%s %s', ErrorMsg, MissingFDs);
+    ExportData.Errors(i) =  convertCharsToStrings(ErrorMsg);
+    continue
+  end
+  
+  
+  % We pull the time series for the three codes.
+  currOpenData = MasterStructure(currOpenCode == CodeCheck).TimeSeries;
+  currClosedData = MasterStructure(currClosedCode == ...
+    CodeCheck).TimeSeries;
+  currCommandData = MasterStructure(currCommandCode == ...
+    CodeCheck).TimeSeries;
+  
+  % We normalize open state data, as necessary.
+  if max(currOpenData.Data) > 1
+    currOpenData.Data = currOpenData.Data ./ max(currOpenData.Data);
+  end
+  
+  % We normalize closed state data, as necessary.
+  if max(currClosedData.Data) > 1
+    currClosedData.Data = currClosedData.Data ./ max( ...
+      currClosedData.Data);
+  end
+  
+  % We normalize command data, as necessary.
+  if max(currCommandData.Data) > 1
+    currCommandData.Data = currCommandData.Data ./ max( ...
+      currCommandData.Data);
+  end
+  
+  % We verify that all three time series begin at the same time stamp.
+  if currOpenData.Time(1) ~= currCommandData.Time(1) || ...
+      currClosedData.Time(1) ~= currCommandData.Time(1)
+    ExportError = ['Datum times are inconsistent across data. ' ...
+      'Cannot compute timing.'];
+    ExportData.Errors(i) = convertCharsToStrings(ExportError);
+    continue
+  end
+  
+  % We assign logical values to TRUE and FALSE states depending on the
+  % state signal structure, which may vary from valve-to-valve.
+  TRUE = GroupList{i,'TRUE State Signal'};
+  FALSE = TRUE == 0;
+  
+  % We look for instances of OPEN -> CLOSE commands.
+  CommandCloseIndices = find(currCommandData.Data == 0);
+  ClosedStateOpenIndices = find(currClosedData.Data == TRUE);
+  CommandSwitchIndices = zeros(1,3);
+  StateSwitchIndices = zeros(1,3);
+  AveragingVector = zeros(1,3);
+  
+  for j = 1:length(CommandCloseIndices)
+    if CommandCloseIndices(j) == 1
+      continue
+    elseif currCommandData.Data(CommandCloseIndices(j) - 1) == 1
+      for k = 1:length(CommandSwitchIndices)
+        if CommandSwitchIndices(k) == 0
+          CommandSwitchIndices(k) = CommandCloseIndices(j) - 1;
+          break
         end
+      end
     end
-
-    for j = 1:length(ClosedStateOpenIndices)
-        if ClosedStateOpenIndices(j) == 1
-            continue
-        elseif currClosedData.Data(ClosedStateOpenIndices(j) - 1) == FALSE
-            for k = 1:length(StateSwitchIndices)
-                if StateSwitchIndices(k) == 0
-                    StateSwitchIndices(k) = ClosedStateOpenIndices(j);
-                    break
-                end
-            end
+  end
+  
+  for j = 1:length(ClosedStateOpenIndices)
+    if ClosedStateOpenIndices(j) == 1
+      continue
+    elseif currClosedData.Data(ClosedStateOpenIndices(j) - 1) == FALSE
+      for k = 1:length(StateSwitchIndices)
+        if StateSwitchIndices(k) == 0
+          StateSwitchIndices(k) = ClosedStateOpenIndices(j);
+          break
         end
+      end
     end
-
-    for j = 1:length(AveragingVector)
-        AveragingVector(j) = 0.1*(StateSwitchIndices(j) - ...
-            CommandSwitchIndices(j));
-    end
-
-    ExportData{i,'Close Time [s]'} = mean(AveragingVector);
-
-    % We look for instances of CLOSE -> OPEN commands.
-    CommandOpenIndices = find(currCommandData.Data == 1);
-    OpenStateOpenIndices = find(currOpenData.Data == TRUE);
-    CommandSwitchIndices = zeros(1,3);
-    StateSwitchIndices = zeros(1,3);
-    AveragingVector = zeros(1,3);
-
-    for j = 1:length(CommandOpenIndices)
-        if CommandOpenIndices(j) == 1
-            continue
-        elseif currCommandData.Data(CommandOpenIndices(j) - 1) == 0
-            for k = 1:length(CommandSwitchIndices)
-                if CommandSwitchIndices(k) == 0
-                    CommandSwitchIndices(k) = CommandOpenIndices(j) - 1;
-                    break
-                end
-            end
+  end
+  
+  for j = 1:length(AveragingVector)
+    AveragingVector(j) = 0.1*(StateSwitchIndices(j) - ...
+      CommandSwitchIndices(j));
+  end
+  
+  ExportData{i,'Close Time [s]'} = mean(AveragingVector);
+  
+  % We look for instances of CLOSE -> OPEN commands.
+  CommandOpenIndices = find(currCommandData.Data == 1);
+  OpenStateOpenIndices = find(currOpenData.Data == TRUE);
+  CommandSwitchIndices = zeros(1,3);
+  StateSwitchIndices = zeros(1,3);
+  AveragingVector = zeros(1,3);
+  
+  for j = 1:length(CommandOpenIndices)
+    if CommandOpenIndices(j) == 1
+      continue
+    elseif currCommandData.Data(CommandOpenIndices(j) - 1) == 0
+      for k = 1:length(CommandSwitchIndices)
+        if CommandSwitchIndices(k) == 0
+          CommandSwitchIndices(k) = CommandOpenIndices(j) - 1;
+          break
         end
+      end
     end
-
-    for j = 1:length(OpenStateOpenIndices)
-        if OpenStateOpenIndices(j) == 1
-            continue
-        elseif currOpenData.Data(OpenStateOpenIndices(j) - 1) == FALSE
-            for k = 1:length(StateSwitchIndices)
-                if StateSwitchIndices(k) == 0
-                    StateSwitchIndices(k) = OpenStateOpenIndices(j);
-                    break
-                end
-            end
+  end
+  
+  for j = 1:length(OpenStateOpenIndices)
+    if OpenStateOpenIndices(j) == 1
+      continue
+    elseif currOpenData.Data(OpenStateOpenIndices(j) - 1) == FALSE
+      for k = 1:length(StateSwitchIndices)
+        if StateSwitchIndices(k) == 0
+          StateSwitchIndices(k) = OpenStateOpenIndices(j);
+          break
         end
+      end
     end
-
-    for j = 1:length(AveragingVector)
+  end
+  
+  for j = 1:length(AveragingVector)
     AveragingVector(j) = 0.1*(StateSwitchIndices(j) - ...
       CommandSwitchIndices(j));
   end
