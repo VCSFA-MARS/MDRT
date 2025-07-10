38eea40c64e6e1fb9ce6f3dab521c8f60d03f1b2
Austin Thomas <austin.thomas@vaspace.org>
2025-07-10 10:30:20 -0400

    significant progress on relief valve selector function

--- Relief Valve Selector/ReliefValveSelectorFunc.m
+++ Relief Valve Selector/ReliefValveSelectorFunc.m
@@ -1,37 +1,4 @@
 function [ModelNum,MinDischargeArea,ActualDischargeArea] = ...
-    ReliefValveSelectorFunc(Commodity,Cv,PressIn,PressSet,Temp,Z, ...
+    ReliefValveSelectorFunc(Commodity,Cv,MAWP,Temp,SetPressure,Z, ...
     PressOver,Kc)
-% -------------------------------------------------------------------------
-
-
-% -------------------------------------------------------------------------
-% We define constants.
-% -------------------------------------------------------------------------
-PressBack = 0;
-% -------------------------------------------------------------------------
-
-
-% -------------------------------------------------------------------------
-% We create a lookup table with constant values for possible commodities.
-% -------------------------------------------------------------------------
-CommodityRef = table('VariableNames',['Commodity','State', ...
-    'SpecificGravity','MolWeight','SpecificHeatRatio'],'VariableTypes', ...
-    ['char','char','double','double','double']);
-CommodityRef.Commodity = ['GN2','GHe'];
-CommodityRef.
-CommodityRef.SpecificGravity = [0.967,0.138];
-CommodityRef.MolWeight = [28.0134,4.002602];
-CommodityRef.SpecificHeatRatio = [1.40,1.66];
-
-
-% -------------------------------------------------------------------------
-% We calculate the required effective discharge area (MinDischargeArea).
-% -------------------------------------------------------------------------
-
-
-
-% -------------------------------------------------------------------------
-% We close the function.
-% -------------------------------------------------------------------------
-end
 % -------------------------------------------------------------------------
