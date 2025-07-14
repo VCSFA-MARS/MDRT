70ce782960fa969f90ad31367b914c9023bfe2dd
Austin Thomas <austin.thomas@vaspace.org>
2025-07-14 12:01:02 -0400

    Removed unnecessary files from repo. Saved locally or deleted.

--- Valve Timing Script/FuncTest.m
+++
@@ -1,8 +0,0 @@
-clear;clc;
-close all force
-
-DataPath = ['C:\Users\AustinThomas\Desktop\data\import\' ...
-    'TestData_Morning\data'];
-ExportFileName = 'ValveTimingTestResults.mat';
-
-ValveTimingFunc(DataPath,ExportFileName)
