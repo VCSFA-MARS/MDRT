ae19f1ab2121f673d24c7cb112f64820d0b30c6d
Nick Counts <nick.counts@gmail.com>
2025-08-04 14:19:26 -0400

    MDRT updated with new GUI tabs: Archive Manager, Settings, Import

--- ApplicationModules/mdrt.m
+++ ApplicationModules/mdrt.m
@@ -1,14 +1,14 @@
-mainGui = figure;
-    guiSize = [672 387 + 25];
-    mainGui.Position = [mainGui.Position(1:2) guiSize];
+mainGui = uifigure;
+    % guiSize = [672 387 + 25];
+    % mainGui.Position = [mainGui.Position(1:2) guiSize];
     mainGui.Name = 'Data Comparison Plotter';
     mainGui.NumberTitle = 'off';
     mainGui.MenuBar = 'none';
     mainGui.ToolBar = 'none';
         
+mainGuiGrid = uigridlayout(mainGui, [1 1]);
         
-        
-tgroup = uitabgroup('Parent', mainGui);
+tgroup = uitabgroup('Parent', mainGuiGrid);
 tab1 = uitab('Parent', tgroup, 'Title', 'Data Viewer');
 tab1a = uitab('Parent', tgroup, 'Title', 'Axes Setup');
 tab2 = uitab('Parent', tgroup, 'Title', 'Import Data');
@@ -18,8 +18,8 @@ tab5 = uitab('Parent', tgroup, 'Title', 'Settings');
 
 
 setTimeAxesLimits( tab1a );
-makeDataImportGUI( tab2 );
-makeArchiveManagerGUI( tab3 );
-makeDataComparisonGUI( tab4 );
-makeSettingsGUI( tab5 );
+dataImportGUI( tab2 );
+ArchiveManagerGUI( Parent=tab3 );
+% makeDataComparisonGUI( tab4 );
+SettingsGUI( tab5 );
 
